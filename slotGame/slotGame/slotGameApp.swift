edcc56041666f112fdcc6552a617c2ce8b165765
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-10-11 10:33:13 -0400

    documents added

---
+++ slotGame/slotGame/slotGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotGameApp.swift
+//  slotGame
+//
+//  Created by Ayça ege Kayhan on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct slotGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
